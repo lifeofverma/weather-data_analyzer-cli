da52758da488a7ad30b35f8fa2674d0080b7d279
Aman Verma <amananimator6@gmail.com>
2024-09-07 14:14:41 +0530

    working on avrage temprature

--- weather_cli.py
+++ weather_cli.py
@@ -1,43 +1,34 @@
 import json
 
+
+new_dict = {}
+
 with open("weather.json", 'r') as file:
-    file_data = json.load(file)
+    json_file_data = json.load(file)
 
-    new_dict = {}
+    for data in json_file_data:
+        if data["city"] not in new_dict:
+            new_dict[data["city"]] = [data["temperature"]]
+        else:
+            new_dict[data["city"]].append(data["temperature"]) 
 
-    for entries in file_data:
-        for key , value in entries.items():
-            print(type(x))
-
-
+    
+for each_cities , temperature in new_dict.items():
+    print (each_cities , average =  sum(temperature))
+            
+        
 
 
 
 """
-    for cities in data:
-        total_cities.append(cities["city"])
-  
-    for i in data:
-        if i["city"] in total_cities:
-            print(i)
-
-        if i in new_dict:
-        new_dict[i] += 1
-    else:
-        new_dict[i] = 1
-    print(list(y))
-    celsius = (Los_Angeles - 32) * 5/9
-    print (celsius)
-    y = set(x)
-            for b in data:
-            if b["city"] in a:
-    print(list(y))
-
-            x.append(i["city"])
-
-        y = set(x)
-        a = list(y)
-
-        print(city , temp )
+else:
+            print("test")
+            
+    print(new_dict)
 """
+
+
+
+
+
 
