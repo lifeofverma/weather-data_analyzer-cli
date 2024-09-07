18a5487f389fd44e1e50a4c4701e82771a535b3a
Aman Verma <amananimator6@gmail.com>
2024-09-07 20:33:24 +0530

    updated expected results as mentioned in document

--- weather_cli.py
+++ weather_cli.py
@@ -3,6 +3,8 @@ import sys
 
 new_dict = {}  # Initialize an empty dictionary to store city names and their temperatures
 
+
+#______________________________________________________________________________________________________________________
 # Open the 'weather.json' file in read mode
 with open("weather.json", 'r') as file: 
     json_file_data = json.load(file)
@@ -18,18 +20,62 @@ with open("weather.json", 'r') as file:
             new_dict[data["city"]].append(data["temperature"]) 
 
 
-
-#average temp
-
+#______________________________________________________________________________________________________________________
+#Default Output: Display average temperatures for all cities in Celsius
 if len(sys.argv) <= 1:
+    print("Average Temperatures:")
     for each_city , temperature in new_dict.items():
         average_temperature = sum(temperature)
         celsius_temprature = (average_temperature - 32) * 5/9
-        print (f"{each_city}{celsius_temprature} celsius")
+        formatted_celsius_temprature = "{:.1f}".format(celsius_temprature)
+        print (f"{each_city}: {formatted_celsius_temprature}")
+    sys.exit(1)
 
-sys.exit(1)
+
+#______________________________________________________________________________________________________________________
 first_arg = sys.argv[1]
-#print(new_dict)
 
+
+
+#______________________________________________________________________________________________________________________
+#List Cities: to display all available cities in the dataset.
+if first_arg == "--list":
+    print("Available Cities:")
+    for each_cities , temperature in new_dict.items():
+        print (f"-{each_cities}")
+    sys.exit(1)
+
+
+#______________________________________________________________________________________________________________________
+#help section writing
+elif first_arg == "--help":
+    print("""
+Weather CLI Tool Usage:
+
+python weather_cli.py [OPTIONS]
+
+This CLI tool reads weather data from a JSON file, calculates the average temperature for each city, writes the results to a new JSON file, and prints the average temperatures in the terminal with colored
+output.
+
+Arguments:
+--help Show this help message and exit.
+--city CITY_NAME Calculate and display the average temperature for the specified city only.
+--convert UNIT Convert temperatures to 'fahrenheit' or 'celsius' (default is Celsius).
+
+Examples:
+python weather_cli.py
+python weather_cli.py --list
+python weather_cli.py --city New York
+python weather_cli.py --convert fahrenheit
+""")
+    sys.exit(1)
+
+
+
+#______________________________________________________________________________________________________________________
+# Handle edge cases where user input does not match any of the valid choices Print a message indicating that the input is invalid or not recognized
+else:
+    print("input is invalid or not recognized\nUse the --help command to see how to use this tool.")
+    sys.exit(1)
 
 
