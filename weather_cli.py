ca8d007d5937f5578c9880fc0644e6f92d6e8129
Aman Verma <amananimator6@gmail.com>
2024-09-07 15:01:19 +0530

    help_section and master edge case updated

--- weather_cli.py
+++ weather_cli.py
@@ -24,20 +24,15 @@ if first_arg == "--list":
     for each_cities , temperature in new_dict.items():
         print (each_cities)
 
+elif first_arg == "--help":
+    print("Weather Analysis Tool - Help Section\n")
+    print("This tool reads weather data from a JSON file, calculates average temperatures for each city, and provides various functionalities for analyzing and displaying the data.")
+    print("Below is an overview of the available commands and features:\n")
+    print("Default Output: Display average temperatures for all cities in Celsius.") 
+    print("Filter by City: Use the --city CITY_NAME argument to display the average temperature for a specific city.")
+    print("Convert Temperatures: Use the --convert fahrenheit argument to display temperatures in Fahrenheit.")
+    print("List Cities: Use the --list argument to display all available cities in the dataset.")
+    print("Help: Use the --help argument to display information about how to use the CLI tool.")
 
-
-
-
-
-#print(new_dict.keys())
-
-
-"""
-if list_cities == "--list":
-    
-
-for each_cities , temperature in new_dict.items():
-    print (each_cities , sum(temperature))
-
-"""     
-
+else:
+    print("use -- help command and this will show you how to use this tool.")
