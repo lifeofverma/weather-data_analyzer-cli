9f7c7007385599f34a8fa700fcaeb0b794c13a30
Aman Verma <amananimator6@gmail.com>
2024-09-12 21:51:02 +0530

    All the logics of programm finished - "Continue working on coloroma, saving files features"

--- weather_cli.py
+++ weather_cli.py
@@ -4,10 +4,6 @@ import sys
 formatted_dict = {}  # Initialize an empty dictionary to store city names and their temperatures
 average_temperature_dict = {}  # Initialize an empty dictionary to store average temperatures of cities
 
-
-
-
-
 #______________________________________________________________________________________________________________________
 # Open the 'weather.json' file in read mode
 with open("weather.json", 'r') as file: 
@@ -23,27 +19,17 @@ with open("weather.json", 'r') as file:
             # If the city already exists in the dictionary, append the new temperature to the city's list
             formatted_dict[data["city"]].append(data["temperature"]) 
 
-
-
-
-
 #______________________________________________________________________________________________________________________
 # Formatting average temperature and adding the values into the average_temperature dictionary
 for each_city , temperature in formatted_dict.items():
     average_temperature = sum(temperature)
     average_temperature_dict[each_city] = average_temperature
 
-
-
-
 #______________________________________________________________________________________________________________________
 #Default Output: Display average temperatures for all cities in Celsius
 if len(sys.argv) <= 1:
     print("Average Temperatures:")
     for each_city , temperature in average_temperature_dict.items():
-        #average_temperature = sum(temperature)
-        #celsius_temprature = (average_temperature - 32) * 5/9
-        #formatted_celsius_temprature = "{:.1f}".format(celsius_temprature)
         print (f"{each_city}: {temperature}")
     sys.exit(1)
 
@@ -53,19 +39,16 @@ first_arg = sys.argv[1]
 
 #______________________________________________________________________________________________________________________
 #List Cities: to display all available cities in the dataset.
-if len(sys.argv) >= 1:
+if len(sys.argv) == 2:
     if first_arg == "--list":
         print("Available Cities:")
         for each_cities , temperature in formatted_dict.items():
             print (f"-{each_cities}")
         sys.exit(1)
 
-
-
-
 #______________________________________________________________________________________________________________________
 #help section writing
-if len(sys.argv) >= 1:
+if len(sys.argv) == 2:
     if first_arg == "--help":
         print("""
     Weather CLI Tool Usage:
@@ -88,11 +71,8 @@ if len(sys.argv) >= 1:
     """)
         sys.exit(1)
 
-
-
-
 #______________________________________________________________________________________________________________________
-if len(sys.argv) > 2:
+if len(sys.argv) == 3:
     second_arg = sys.argv[2]
     if first_arg == "--city":
         if second_arg in average_temperature_dict.keys():
@@ -104,19 +84,26 @@ if len(sys.argv) > 2:
             print("Input city is not available.\nPlease refer to the command <--list> to check available cities")
             sys.exit(1)
 
-if len(sys.argv) > 2:
+#______________________________________________________________________________________________________________________
+if len(sys.argv) == 3:
     second_arg = sys.argv[2]
     if first_arg == "--convert" and second_arg == "fahrenheit":
         print("Average Temperatures:")
-        for each_city , temperature in formatted_dict.items():
-            average_temperature = sum(temperature)
-            celsius_temprature = (average_temperature - 32) * 5/9
-            fahrenheit_temperature = (celsius_temprature * 1.8) + 32
-            print (f"{each_city}: {fahrenheit_temperature}")
+        for each_city , temperature in average_temperature_dict.items():
+            fahrenheit_temperature = (temperature * 1.8) + 32
+            formatted_fahrenheit_temperature = "{:.1f}".format(fahrenheit_temperature)
+            print (f"{each_city}: {formatted_fahrenheit_temperature}")
         sys.exit(1)
+
+#______________________________________________________________________________________________________________________
+if len(sys.argv) > 2 :
+    print("input is invalid or not recognized\nUse the --help command to see how to use this tool.")
+    sys.exit(1)
 
 #______________________________________________________________________________________________________________________
 # Handle edge cases where user input does not match any of the valid choices Print a message indicating that the input is invalid or not recognized
 else:
     print("input is invalid or not recognized\nUse the --help command to see how to use this tool.")
     sys.exit(1)
+
+
